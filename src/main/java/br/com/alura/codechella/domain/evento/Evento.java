c34ba51a4f157e7e888359de37db19c2ccf0e766
drikones <adriano_rocha15@hotmail.com>
2024-12-14 22:23:40 -0300

    Atuando no design estratégico

---
+++ src/main/java/br/com/alura/codechella/domain/evento/Evento.java
@@ -0,0 +1,46 @@
+package br.com.alura.codechella.domain.evento;
+
+import br.com.alura.codechella.Categoria;
+import br.com.alura.codechella.domain.ingresso.TipoIngresso;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Evento {
+    private Categoria categoria;
+    private String descricao;
+    private LocalDateTime data;
+    private Endereco endereco;
+    private List<TipoIngresso> tiposIngresso = new ArrayList<>();
+
+
+    public void incluiNovoTipoDeIngressoAoEvento(TipoIngresso tipoIngresso) {
+        tiposIngresso.add(tipoIngresso);
+    }
+
+    public void aumentaNumeroDeIngressosPorTipo() {
+        //implementar aqui
+    }
+
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public List<TipoIngresso> getTiposIngresso() {
+        return tiposIngresso;
+    }
+}
